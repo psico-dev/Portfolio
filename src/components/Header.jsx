7ef140192264a3f6e133cf0d3dff1bbc8f3aaff2
Andrés Pérez <alejoandi@hotmail.com>
2022-03-25 09:18:58 -0500

    Maquetacion y paginacion

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Header () {
+  return(
+    <header>
+      
+      <div className="box-logo">
+        <img src="" alt="logo Andrés" />
+        <p>Andrés</p>
+        <p>Web Developer</p>
+      </div>
+
+      <div className="header-list">
+        <ul>
+          <li>Sobre mi</li>
+          <li>Skills</li>
+          <li>Proyectos</li>
+          <li>Contáctame</li>
+        </ul>
+      </div>
+    </header>
+  )
+}
+
+export { Header }
---
+++ src/routes/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "../components/Header"
+
+function App () {
+  return(
+    <>
+      <Header />
+      <h1>
+        Hola 
+      </h1>
+    </>
+  )
+}
+
+export { App }
