716ceedbc0436b45a68bce35fcc2ea2a3eb39926
Andrés Pérez <alejoandi@hotmail.com>
2022-06-13 17:26:22 -0500

    netlify

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -5,12 +5,12 @@ function Contact () {
   return(
     <section className="contact">
       <h2>Contáctame</h2>
-      <form action="submit" name="contact" className="form" netlify>
+      <form name="contact" className="form" netlify>
         <label htmlFor="name">Nombre: </label>
         <input type="text" id="name" placeholder="Nombre" />
         <label htmlFor="email">Correo: </label>
         <input id="email" type="email" placeholder="Correo" />
-        <label htmlFor="message">Mensaje: </label>
+        <label htmlFor="message">Mensaje:</label>
         <textarea name="message" id="message" cols="20" rows="10" placeholder="Deja tu mensaje"></textarea>
         <button type="submit">Enviar</button>
       </form>
